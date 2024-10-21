475e7797445199275d84f1cb1ca95f1f24c2c129
Atharv <harshubhople1@gmail.com>
2024-10-21 21:46:54 +0530

    transactions-app

---
+++ app/backend/app.js
@@ -0,0 +1,20 @@
+// const express = require('express');
+// const cors = require('cors');
+// app.use(cors());
+// ;
+// const connectDB = require('./config/db');
+// const transactionRoutes = require('./routes/transactionRoutes');
+
+// const app = express();
+
+// connectDB();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/api', transactionRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
